72f901259b31a7caaded9af8900c4f2d34f68510
Jake <jacob.m.livingston@gmail.com>
2023-07-11 13:45:58 -0700

    Update narrative backend system
    - Add character scriptable object
    - Update narrative item object - Now will have more options for unity specific systems, like buttons and images, and audio
    - Created the basic narrative manager system to allow us to have a callback for advancing the narrative (includes playing audio)

--- Assets/Scripts/NarrationItem.cs
+++ Assets/Scripts/NarrationItem.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
+
 [CreateAssetMenu(fileName = "Narrative", menuName = "Narrative/Narrative Item")]
 public class NarrationItem : ScriptableObject{
     [Tooltip("Day the narration occurs")]
@@ -9,18 +11,20 @@ public class NarrationItem : ScriptableObject{
     [Tooltip("Character who is speaking")]
     public Character character;
     [Tooltip("Sounds that play in order from the beginning of the narration")]
-    public List<AudioSource> sound;
+    public List<AudioClip> sounds;
     [TextArea,Tooltip("Text that is spoken by the character")]
     public string line;
     [Tooltip("Next Narrative item. 1 or more")]
     public List<NextNarrative> next;
+    [Tooltip("Image to be displayed behind the characters")]
+    public Image background;
 
 }
 
 [Serializable]
 public class NextNarrative {
     [SerializeField, Tooltip("What triggers the next narration.")]
-    public TriggerItem        trigger;
+    public Button        button;
     [SerializeField, Tooltip("The next narration to occur")]
     public NarrationItem narrativeItem;
 }
--- Assets/Scripts/TriggerItem.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class TriggerItem : ScriptableObject {
-    public Button button;
-    // publ
-}
