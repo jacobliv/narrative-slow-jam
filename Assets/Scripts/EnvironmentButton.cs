899cbc9db2e109aea60f6f060b73691504849845
Jake <jacob.m.livingston@gmail.com>
2023-07-20 10:13:28 -0700

    Added background, most sfx, fonts

--- Assets/Scripts/EnvironmentButton.cs
+++ Assets/Scripts/EnvironmentButton.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnvironmentButton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EnvironmentButton : MonoBehaviour {
+    [SerializeField] private List<AudioClip> sounds;
+    [SerializeField] private AudioSource     audioSource;
+    [SerializeField] private Button          button;
+    void Start() {
+        button.onClick.AddListener(PlaySfx);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void PlaySfx() {
+        audioSource.Stop();
+        audioSource.clip = sounds[Random.Range(0, sounds.Count - 1)];
+        Debug.Log(audioSource.clip.name);
+        audioSource.Play();
     }
 }
