d4ea6aee81ea4531a5e4e65d61dc4f611e9bc581
Jake <jacob.m.livingston@gmail.com>
2023-07-22 08:49:14 -0700

    Create all the characters

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "Narrative", menuName = "Narrative/Character")]
+[CreateAssetMenu(fileName = "Character", menuName = "Narrative/Character")]
 
 public class Character : ScriptableObject {
     [Tooltip("Name of character")]
