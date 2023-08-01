f534e4d5f5fcd25260adb84fc9c6c871e512ce7c
Jake <jacob.m.livingston@gmail.com>
2023-08-01 09:41:47 -0700

    When going to main menu, background music should revert to normal music

--- Assets/Scripts/ControlBackgroundMusic.cs
+++ Assets/Scripts/ControlBackgroundMusic.cs
@@ -4,31 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlBackgroundMusic : MonoBehaviour {
-    public AudioSource          source;
-    public List<BackgroundSong> backgroundSongs;
-    public AudioClip            ending;
-    void Start() {
-        
+    public AudioSource source;
+    public AudioClip   beginning;
+    public AudioClip   ending;
+    public void ChangeToStartSong() {
+        source.Stop();
+        source.clip = beginning;
+        source.Play();
     }
-
 
     public void ChangeToEndSong() {
         source.Stop();
         source.clip = ending;
         source.Play();
     }
-    public void PlaySong(Day day) {
-        source.Stop();
-        source.clip = backgroundSongs.Find((a) => a.day == day).song;
-        source.Play();
-    }
-    
-}
 
-[Serializable]
-public class BackgroundSong {
-    [SerializeField]
-    public Day       day;
-    [SerializeField]
-    public AudioClip song;
+    
 }
--- Assets/Scripts/SwitchOnClick.cs
+++ Assets/Scripts/SwitchOnClick.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SwitchOnClick : MonoBehaviour {
-
+    public UnityEvent onClick;
     void Update() {
         if (Input.anyKey) {
             SceneManager.LoadScene("StartMenu");
-
+            onClick.Invoke(); 
         }
 
     }
